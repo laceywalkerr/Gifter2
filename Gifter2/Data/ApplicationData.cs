03ad72c4cf7d9e321f20c6cf4addb0ff9424eb30
Lacey Walker <bluelarksong@gmail.com>
2021-01-11 11:30:22 -0600

    first commit

---
+++ Gifter2/Data/ApplicationData.cs
@@ -0,0 +1,19 @@
+﻿using Gifter2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter2.Data
+{
+    public class ApplicationData
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+            public DbSet<UserProfile> UserProfile { get; set; }
+            public DbSet<Post> Post { get; set; }
+        }
+    }
+}
---
+++ Gifter2/Repositories/PostRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter2.Repositories
+{
+    public class PostRepository
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class PostController : ControllerBase
+        {
+            private readonly IPostRepository _postRepository;
+            public PostController(IPostRepository postRepository)
+            {
+                _postRepository = postRepository;
+            }
+
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(_postRepository.GetAll());
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult Get(int id)
+            {
+                var post = _postRepository.GetById(id);
+                if (post == null)
+                {
+                    return NotFound();
+                }
+                return Ok(post);
+            }
+        }
+    }
+}
