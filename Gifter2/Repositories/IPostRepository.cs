bd7fc6cb0dee2eb10ca212083d93b043b9c10281
Lacey Walker <bluelarksong@gmail.com>
2021-01-13 03:06:57 -0600

    fixed changes that wouldnt save prior

--- Gifter2/Repositories/IPostRepository.cs
+++ Gifter2/Repositories/IPostRepository.cs
@@ -5,7 +5,11 @@ namespace Gifter2.Repositories
 {
     public interface IPostRepository
     {
+        void Add(Post post);
+        void Delete(int id);
         List<Post> GetAll();
         Post GetById(int id);
+        List<Post> GetByUserProfileId(int id);
+        void Update(Post post);
     }
 }
