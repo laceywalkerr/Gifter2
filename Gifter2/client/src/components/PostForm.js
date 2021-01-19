89b029e0e7464ebc6b5ab3ce04ec0dca0f848a69
Lacey Walker <bluelarksong@gmail.com>
2021-01-19 00:45:53 -0600

    got rid of bug

--- Gifter2/client/src/components/PostForm.js
+++ Gifter2/client/src/components/PostForm.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import Post from './Post';
 
 const PostForm = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        fetch('/api/post/add')
+        fetch('/api/post')
             .then(res => res.json())
             .then(data => setPosts(data));
     }, []);
@@ -13,11 +12,16 @@ const PostForm = () => {
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="cards-column">
-                    {posts.map((post) => (
-                        <Post key={post.id} post={post} />
-                    ))}
-                </div>
+                {/* <div className=""> */}
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <img src={post.imageUrl} alt={post.title} />
+                        <p>
+                            <strong>{post.title}</strong>
+                        </p>
+                        <p>{post.caption}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
