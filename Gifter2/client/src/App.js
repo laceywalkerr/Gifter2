e3ef70a17c696ecddc20405e9302f00587feb52e
Lacey Walker <bluelarksong@gmail.com>
2021-01-19 01:36:17 -0600

    working through chapter 6

--- Gifter2/client/src/App.js
+++ Gifter2/client/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
-import PostSearch from "./components/PostSearch"
+// import PostForm from "./components/PostForm";
+// import PostList from "./components/PostList";
+// import PostSearch from "./components/PostSearch"
+import ApplicationViews from "./components/ApplicationViews";
 
 function App() {
   return (
     <div className="App">
-      <PostForm />
+      <Router>
+        <ApplicationViews />
+      </Router>
+      {/* <PostForm />
       <PostList />
-      <PostSearch />
+      <PostSearch /> */}
     </div>
   );
 }
 
 export default App;
+
