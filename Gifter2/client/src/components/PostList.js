79b5a36b8cd4d7a98fd3f0e6fc01b6add507470f
Lacey Walker <bluelarksong@gmail.com>
2021-01-19 00:15:50 -0600

    THE GIFS APPEAR

--- Gifter2/client/src/components/PostList.js
+++ Gifter2/client/src/components/PostList.js
@@ -4,7 +4,7 @@ const PostList = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        fetch('/api/posts')
+        fetch('/api/post')
             .then(res => res.json())
             .then(data => setPosts(data));
     }, []);
@@ -26,5 +26,33 @@ const PostList = () => {
         </div>
     );
 };
+
+
+// const PostList = () => {
+//     const [posts, setPosts] = useState([]);
+
+//     useEffect(() => {
+//         fetch('/api/posts')
+//             .then(res => res.json())
+//             .then(data => setPosts(data));
+//     }, []);
+
+//     return (
+//         <div className="container">
+//             <div className="row justify-content-center">
+//                 {/* <div className=""> */}
+//                 {posts.map((post) => (
+//                     <div key={post.id}>
+//                         <img src={post.imageUrl} alt={post.title} />
+//                         <p>
+//                             <strong>{post.title}</strong>
+//                         </p>
+//                         <p>{post.caption}</p>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// };
 
 export default PostList;
